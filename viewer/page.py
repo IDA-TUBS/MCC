ba3d413fa219ef3e6be621ef3d95956dd68e7e59
Johannes Schlatow <schlatow@ida.ing.tu-bs.de>
2019-02-18 17:11:02 +0100

    viewer: revise param view and implement highlighting

--- viewer/page.py
+++ viewer/page.py
@@ -4,6 +4,7 @@ import re
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
+from gi.repository import Pango
 
 from xdot.ui.window import DotWidget
 from xdot.ui.elements import Graph
@@ -11,6 +12,56 @@ from xdot.ui.elements import Graph
 from mcc.dot import DotFactory
 from mcc.framework import Registry
 from mcc.importexport import PickleImporter
+from mcc import graph as mccgraph
+
+class ModelItem():
+    def __init__(self, layer, obj):
+        self.layer = layer
+        self.obj   = obj
+
+
+class ParamView():
+    def __init__(self):
+
+        model     = Gtk.TreeStore(object, str, int, Pango.Style, str, Pango.Underline, bool, str)
+        self.view = Gtk.TreeView.new_with_model(model)
+
+        col = Gtk.TreeViewColumn("No Node selected", Gtk.CellRendererText(),
+                                 text=1, weight=2, style=3, foreground=4, underline=5, strikethrough=6, background=7)
+        self.view.append_column(col)
+
+        self.default_style = { 'weight'     : 400,
+                               'style'      : Pango.Style.NORMAL,
+                               'foreground' : 'black',
+                               'underline'  : Pango.Underline.NONE,
+                               'strikethrough' : False,
+                               'background' : 'white' }
+
+    def treeview(self):
+        return self.view
+
+    def add_treenode(self, parent, text, style, extra_style=None, link_item=None):
+        # merge style with defaults by overwriting defaults values with those of 'style'
+        if extra_style is not None:
+            combined_style = {**extra_style, **style}
+        else:
+            combined_style = style
+        combined_style = {**self.default_style, **combined_style}
+
+        return self.view.get_model().append(parent, [link_item,
+                                                    text, combined_style['weight'],
+                                                          combined_style['style'],
+                                                          combined_style['foreground'],
+                                                          combined_style['underline'],
+                                                          combined_style['strikethrough'],
+                                                          combined_style['background']])
+
+    def clear(self):
+        self.view.get_model().clear()
+
+    def refresh(self, title):
+        self.view.expand_all()
+        self.view.get_column(0).set_title(title)
 
 class Page(Gtk.HPaned):
 
@@ -36,12 +87,13 @@ class Page(Gtk.HPaned):
             self.combo.append(choice, choice)
         self.combo.connect('changed', self.show_dot)
 
-        self.paramview = Gtk.TreeView.new_with_model(Gtk.TreeStore(str, int))
-        col = Gtk.TreeViewColumn("No Node selected", Gtk.CellRendererText(),
-                                 text=0, weight=1)
-        self.paramview.append_column(col)
+        self.paramview = ParamView()
+
         scroll = Gtk.ScrolledWindow()
-        scroll.add_with_viewport(self.paramview)
+        scroll.add_with_viewport(self.paramview.treeview())
+
+        select = self.paramview.treeview().get_selection()
+        select.connect('changed', self.on_tree_selection_changed)
 
         self.sidepane.pack_start(self.combo, False, False, 0)
         self.sidepane.pack_end(scroll, True, True, 0)
@@ -118,35 +170,111 @@ class Page(Gtk.HPaned):
             return frozenset(map(self._gen_hashable_value, value))
         return repr(value)
 
+    def _stylize(self, param, candidates, value):
+        style = dict()
+        name = param
+        if param in self.model.by_name.keys():
+            # use italic for inter-layer relations
+            style['style'] = Pango.Style.ITALIC
+
+            if self.model.by_name[param] == self._parent_layer():
+                name = 'Parent layer (%s)' % param
+            elif self.model.by_name[param] == self._child_layer():
+                name = 'Child layer (%s)' % param
+            else:
+                name = 'Layer (%s)' % param
+
+        if len(candidates) == 0:
+            # use normal font weight if there was no decision
+            style['weight'] = 400
+
+        if value is None or value == 'None':
+            # strikethrough if no value assigned
+            style['strikethrough'] = True
+
+        return name, style
+
+    def _link(self, layer, value):
+        if value in layer.graph.nodes():
+            return ModelItem(layer, value)
+
+        if value in layer.graph.edges():
+            return ModelItem(layer, value)
+
+        return None
+
+    def _add_value(self, parent, value, layer, style, extra_style=None):
+        if type(value) not in {set, frozenset}:
+            self.paramview.add_treenode(parent, repr(value), style, extra_style,
+                                        link_item=self._link(layer, value))
+        else:
+            setnode = self.paramview.add_treenode(parent,
+                                                  'List',
+                                                  style,
+                                                  extra_style)
+            for element in value:
+                self.paramview.add_treenode(setnode, repr(element), style, extra_style,
+                                            link_item=self._link(layer, element))
+
     def _add_paramtree(self, parent, params):
-        # TODO filter out params that store inter-layer relations
+        # TODO toggle params that store inter-layer relations
         for name, content in params.items():
-            node = self._add_treenode(parent, name)
 
-            value = self._gen_hashable_value(content['value'])
-            candidates = self._gen_hashable_value(content['candidates'])
+            value      = content['value']
+            candidates = content['candidates']
 
-            #Make sure that the selected value is in the possibly empty set of
-            #candidates.
-            for candidate in candidates | {value}:
-                selected = candidate == value
+            layer = self._current_layer()
+            if name in self.model.by_name.keys():
+                layer = self.model.by_name[name]
 
-                if frozenset != type(candidate):
-                    self._add_treenode(node, candidate, selected)
+            name, style = self._stylize(name, candidates, value)
+            node = self.paramview.add_treenode(parent, name, style, {'underline' : Pango.Underline.SINGLE})
+
+            self._add_value(node, value, layer, style, {'weight' : 800})
+
+            for candidate in candidates:
+                if candidate == value:
                     continue
-                setnode = self._add_treenode(node, 'List', selected)
-                for element in candidate:
-                    self._add_treenode(setnode, element, selected)
+                self._add_value(node, candidate, layer, style)
 
-    def _add_treenode(self, parent, text, is_bold=False):
-        weight = 800 if is_bold else 400
-        return self.paramview.get_model().append(parent, [text, weight])
+    def _parent_layer(self):
+        current = self._current_layer()
+        return self.model._prev_layer(current)
+
+    def _child_layer(self):
+        current = self._current_layer()
+        return self.model._next_layer(current)
+
+    def _current_layer(self):
+        return self.model.by_name[self.combo.get_active_id()]
+
+    def on_tree_selection_changed(self, selection):
+        model, treeiter = selection.get_selected()
+        if treeiter is not None and model[treeiter][0] is not None:
+            item = model[treeiter][0]
+            if isinstance(item.obj, mccgraph.Edge):
+                # TODO edge object do not have an URL in upstream xdot
+                url = item.layer.graph.edge_attributes(item.obj)['id']
+            else:
+                url = item.layer.graph.node_attributes(item.obj)['id']
+
+            found = False
+            for element in self.dotwidget.graph.nodes + self.dotwidget.graph.edges:
+                if element.url == url:
+                    found = True
+                    self.dotwidget.set_highlight({element}, search=True)
+
+            if not found:
+                print("Cannot find selected item %s with url %s." % (item, url))
+
+        else:
+            self.dotwidget.set_highlight(None, search=True)
 
     def on_url_clicked(self, widget, url, event):
-        self.paramview.get_model().clear()
+        self.paramview.clear()
 
         # TODO put this into some class (ModelSearch)?
-        current_layer = self.model.by_name[self.combo.get_active_id()]
+        current_layer = self._current_layer()
         # find node
         title = "%s not found in graph" % url
         for node in current_layer.graph.nodes():
@@ -154,15 +282,16 @@ class Page(Gtk.HPaned):
                 #TODO underscores aren't properly rendered
                 title = 'Parameters for %s' % node.label()
 
-                nodetree = self._add_treenode(None, "Node Parameters")
+                nodetree = self.paramview.add_treenode(None, "Node Parameters", dict(),
+                                                       link_item=ModelItem(current_layer, node))
                 params = current_layer.graph.node_attributes(node)['params']
                 self._add_paramtree(nodetree, params)
 
                 for edge in current_layer.graph.out_edges(node):
-                    edgetree = self._add_treenode(None, "Edge Parameters")
+                    edgetree = self.paramview.add_treenode(None, "Edge Parameters", dict(),
+                                                           link_item=ModelItem(current_layer, edge))
                     params = current_layer.graph.edge_attributes(edge)['params']
                     self._add_paramtree(edgetree, params)
 
-        self.paramview.expand_all()
-        self.paramview.get_column(0).set_title(title)
+        self.paramview.refresh(title)
         return True
