32ab5ffb08e14015efeee980edb77e544caa2985
Edgard Schmidt <schmidt@edik.ch>
2019-06-08 01:49:25 +0200

    mcc_c1: allow path setting via cli arguments

--- mcc_c1.py
+++ mcc_c1.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python
 
 import logging
+from argparse import ArgumentParser
 from mcc import c1
 
+def get_args():
+    descr = 'all arguments are optional'
+    parser = ArgumentParser(description=descr)
+    parser.add_argument('filename', default='mcc_control.xml', nargs='?')
+    parser.add_argument('--basepath', default='models/')
+    parser.add_argument('-o', '--outpath', default='/tmp/test-')
+    return parser.parse_args()
+
 if __name__ == '__main__':
+
+    args = get_args()
 
     logging.basicConfig(format='%(levelname)s: %(message)s')
     logging.getLogger().setLevel(logging.INFO)
 
-    mcc = c1.Mcc('mcc_control.xml', basepath='models/', outpath='/tmp/test-')
+    mcc = c1.Mcc(args.filename, basepath=args.basepath, outpath=args.outpath)
     mcc.execute()
